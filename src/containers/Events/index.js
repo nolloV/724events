3f40c9f82d50b9da2397fce25c9056be817bb953
nolloV <142914191+nolloV@users.noreply.github.com>
2024-01-12 10:48:57 -0500

    fix filter and period

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,23 +13,24 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = ((!type ? data?.events : data?.events) || []).filter(
-    (event, index) => {
-      if (
-        (currentPage - 1) * PER_PAGE <= index &&
-        PER_PAGE * currentPage > index
-      ) {
-        return true;
-      }
-      return false;
-    }
+
+  const eventsByType = type
+    ? data?.events.filter((event) => event.type === type)
+    : data?.events;
+
+  const filteredEvents = (eventsByType || []).filter(
+    (event, index) =>
+      (currentPage - 1) * PER_PAGE <= index && PER_PAGE * currentPage > index
   );
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
